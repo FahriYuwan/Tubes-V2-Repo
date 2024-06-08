72d133f19f860d37610ca1405b08ba464baf821c
hanswryy <farhan.hans257@gmail.com>
2024-06-08 14:22:25 +0700

    dunno

--- assets/Scripts/CardManager.cs
+++ assets/Scripts/CardManager.cs
@@ -15,6 +15,26 @@ public class CardManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // constantly check grandchild of this object if there is Grabbable component in there
+        foreach (Transform child in transform)
+        {
+            foreach (Transform grandchild in child)
+            {
+                if (grandchild.GetComponent<Grabbable>() != null)
+                {
+                    // if there is Grabbable component, check if it is grabbed
+                    if (grandchild.GetComponent<Grabbable>().isGrabbed)
+                    {
+                        // if it is grabbed, check if it is in the list of cards
+                        if (cards.Contains(grandchild.GetComponent<Grabbable>().cardNumber))
+                        {
+                            // if it is in the list of cards, remove it from the list
+                            cards.Remove(grandchild.GetComponent<Grabbable>().cardNumber);
+                        }
+                    }
+                }
+            }
+        }
+
     }
 }
