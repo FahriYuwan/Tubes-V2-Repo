0053239f821f04e854e840ca2db74498a17fa23a
hanswryy <farhan.hans257@gmail.com>
2024-06-08 23:36:43 +0700

    card shift on progress

--- assets/Scripts/CardManager.cs
+++ assets/Scripts/CardManager.cs
@@ -6,6 +6,7 @@ public class CardManager : MonoBehaviour
 {
     // list of cards
     public List<int> cards;
+    public bool shift = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,26 +16,35 @@ public class CardManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // constantly check grandchild of this object if there is Grabbable component in there
+        StartCoroutine(checkCards());
+    }
+
+    IEnumerator checkCards()
+    {
+        yield return new WaitForSeconds(0.1f);
+        cards.Clear();
         foreach (Transform child in transform)
         {
             foreach (Transform grandchild in child)
             {
                 if (grandchild.GetComponent<Grabbable>() != null)
                 {
-                    // if there is Grabbable component, check if it is grabbed
-                    if (grandchild.GetComponent<Grabbable>().isGrabbed)
-                    {
-                        // if it is grabbed, check if it is in the list of cards
-                        if (cards.Contains(grandchild.GetComponent<Grabbable>().cardNumber))
-                        {
-                            // if it is in the list of cards, remove it from the list
-                            cards.Remove(grandchild.GetComponent<Grabbable>().cardNumber);
-                        }
-                    }
+                    cards.Add(grandchild.GetComponent<Grabbable>().type);
                 }
             }
         }
+    }
 
+    IEnumerator shiftCards(int index)
+    {
+        yield return new WaitForSeconds(0.1f);
+        // shift all cards from index to the right
+        for (int i = index; i < transform.childCount; i++)
+        {
+            // get the current i card
+            Transform card = transform.GetChild(i);
+            // change its sibling index to i+1
+            card.SetSiblingIndex(i + 1);
+        }
     }
 }
--- assets/Scripts/SlotManager.cs
+++ assets/Scripts/SlotManager.cs
@@ -14,22 +14,30 @@ public class SlotManager : MonoBehaviour, IDropHandler
             Debug.Log("No Grabbable component found");
             return;
         }
-        // set scale to 0.65f
-        droppedObject.transform.localScale = new Vector3(0.35f, 0.35f, 0.35f);
-        Grabbable grabbable = droppedObject.GetComponent<Grabbable>();
-        grabbable.originalParent = transform;
-        // get the card manager from parent
-        CardManager cardManager = transform.parent.GetComponent<CardManager>();
-        // check the card type, insert the int type to the list
-        cardManager.cards.Add(grabbable.type);
-        if (cardManager.cards.Count > 4)
+
+        // check if original parent of dropped object has a SlotManager component
+        if (droppedObject.GetComponent<Grabbable>().originalParent.GetComponent<SlotManager>())
         {
+            Debug.Log("Original parent has SlotManager component");
+        } else {
             // clone this object
             GameObject clone = Instantiate(gameObject, transform.parent);
             // get parent of this object
             Transform parent = transform.parent;
             // add 400 to its width
             parent.GetComponent<RectTransform>().sizeDelta += new Vector2(400, 0);
+        }
+
+        droppedObject.transform.localScale = new Vector3(0.35f, 0.35f, 0.35f);
+        Grabbable grabbable = droppedObject.GetComponent<Grabbable>();
+        grabbable.originalParent = transform;
+
+        if (transform.childCount > 1)
+        {
+            // get the last child
+            Transform lastChild = transform.GetChild(transform.childCount - 1);
+            // change its sibling index to 0
+            lastChild.SetSiblingIndex(0);
         }
     }
 }
